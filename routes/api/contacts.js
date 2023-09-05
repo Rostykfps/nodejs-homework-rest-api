201df52aaa32033de20f4c054125674ca79cd769
Rostykfps <rostikfps@gmail.com>
2023-09-05 17:57:57 +0300

    Add and changed files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,88 @@
-const express = require('express')
+const express = require('express');
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../../models/contacts');
 
-const router = express.Router()
+const { HttpError } = require('../../helpers');
+const { addSchema } = require('../../schemas/contacts');
+
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contacts = await listContacts();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const contactById = await getContactById(contactId);
+
+    if (!contactById) {
+      throw HttpError(404, 'Not found');
+    }
+
+    res.json(contactById);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, 'missing required name field');
+    }
+    const newContact = await addContact(req.body);
+
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const removedContact = await removeContact(contactId);
+
+    if (!removedContact) {
+      throw HttpError(404, 'Not found');
+    }
+
+    res.json({ message: 'contact deleted' });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const body = req.body;
+    if (!body) {
+      throw HttpError(400, 'missing fields');
+    }
 
-module.exports = router
+    const { contactId } = req.params;
+    const updatedContact = await updateContact(contactId, body);
+
+    if (!updatedContact) {
+      throw HttpError(404, 'Not found');
+    }
+
+    res.json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
