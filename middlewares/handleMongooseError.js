3906baab5de870b2e0d7fa5b719fa7b4ff402e41
Rostykfps <rostikfps@gmail.com>
2023-09-22 15:46:24 +0300

    Created users addpoints

--- middlewares/handleMongooseError.js
+++ middlewares/handleMongooseError.js
@@ -1,5 +1,14 @@
 const handleMongooseError = (error, data, next) => {
-  error.status = 400;
+  const { name, code } = error;
+
+  const status = name === 'MongoServerError' && code === 11000 ? 409 : 400;
+
+  error.status = status;
+
+  if (name === 'ValidationError') {
+    error.message = 'Помилка від Joi або іншої бібліотеки валідації';
+  }
+
   next();
 };
 
--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,6 +3,10 @@ const { HttpError } = require('../helpers');
 const validateBody = schema => {
   const func = (req, res, next) => {
     if (!Object.keys(req.body).length) {
+      if (req.method === 'PATCH' && req.originalUrl === '/api/users') {
+        next(HttpError(400, 'missing field subscription'));
+      }
+
       if (req.method === 'PATCH') {
         next(HttpError(400, 'missing field favorite'));
       }
@@ -12,6 +16,9 @@ const validateBody = schema => {
 
     const { error } = schema.validate(req.body);
     if (error) {
+      if (req.route.path === '/register' || req.route.path === '/login') {
+        next(HttpError(400, 'Помилка від Joi або іншої бібліотеки валідації'));
+      }
       next(HttpError(400, error.details[0].message));
     }
     next();
